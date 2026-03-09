449e6a855637b1c17f36ca4259b8c037d24dd095
castaneras <castaneras@atomicmail.io>
2026-03-09 22:19:29 +0100

    Tuple learning

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,17 @@ fn main() {
     println!("{}", x);
     const THIS_IS_A_CONSTANT: f32 = 1.24;
     println!("{}", THIS_IS_A_CONSTANT);
+    // Scalar data types tests
+    let foo = 2; //Untyped number. Integer.
+    let fooo: u8 = 2; //Typer number. Integer. Unsigned (Only positives)
+    let bar = 3.3;
+    let baar: f32 = 4.56;
+    println!("{}{}{}{}{}{}{}", foo, " ", fooo," ", bar," ", baar);
+    let xd: bool = true;
+    let dx: char = 'a';
+    // Compounds
+    let  mut tup = (198, 'A', "Hola a todos".to_string());
+    println!("{}", tup.1);
+    tup.0 = "replaced".to_string();
+    println!("{}", tup.0);
 }
